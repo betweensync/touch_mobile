46ee00121ae6af9d32a115355776be4e1c46f11c
javalove93 <javalove93@gmail.com>
2014-01-05 12:57:34 +0900

    2014-01-05

---
+++ AndroidHelloWorld/src/com/example/helloworld/C.java
@@ -0,0 +1,11 @@
+package com.example.helloworld;
+
+public class C {
+	public final static String ID = "AndroidHelloWorld";
+	public final static int SYNC_INIT = 0;
+	public final static int SYNC_MOBILE_TO_SERVER = 1;
+	public final static int SYNC_SERVER_TO_MOBILE = 2;
+	public final static int SYNC_MERGE = 3;
+	public final static int SYNC_KEEP_MOBILE = 5;
+	public final static int SYNC_KEEP_SERVER = 6;
+}
--- AndroidHelloWorld/src/com/example/helloworld/MainActivity.java
+++ AndroidHelloWorld/src/com/example/helloworld/MainActivity.java
@@ -1,22 +1,23 @@
 package com.example.helloworld;
 
-import java.io.IOException;
+import java.util.Iterator;
+import java.util.List;
 
-import org.codehaus.jackson.JsonNode;
-import org.codehaus.jackson.map.ObjectMapper;
-import org.codehaus.jackson.node.ObjectNode;
 import org.ektorp.CouchDbConnector;
-import org.ektorp.CouchDbInstance;
-import org.ektorp.ReplicationCommand;
-import org.ektorp.ReplicationStatus;
-import org.ektorp.android.util.ChangesFeedAsyncTask;
-import org.ektorp.changes.ChangesCommand;
-import org.ektorp.changes.DocumentChange;
-import org.ektorp.http.HttpClient;
-import org.ektorp.impl.StdCouchDbInstance;
+import org.ektorp.ViewQuery;
+import org.ektorp.ViewResult;
+import org.ektorp.ViewResult.Row;
 
+import android.accounts.Account;
+import android.accounts.AccountManager;
 import android.app.Activity;
+import android.content.ContentResolver;
+import android.content.Context;
+import android.content.Intent;
+import android.database.ContentObserver;
+import android.net.Uri;
 import android.os.Bundle;
+import android.provider.ContactsContract.RawContacts;
 import android.util.Log;
 import android.view.Menu;
 import android.view.MotionEvent;
@@ -24,104 +25,121 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 
-import com.couchbase.touchdb.TDServer;
-import com.couchbase.touchdb.ektorp.TouchDBHttpClient;
-import com.couchbase.touchdb.router.TDURLStreamHandlerFactory;
-
 public class MainActivity extends Activity {
 
-	static {
-		//	TouchDB Initialization
-		TDURLStreamHandlerFactory.registerSelfIgnoreError();
-	}
-	
-	EditText editText1, editText2;
-	Button button1, button2;
-	CouchDbInstance dbInstance;
-	CouchDbConnector dbConnector;
-	String ignoreId = "";
-	ReplicationCommand pushCommand, pullCommand;
-	ReplicationStatus pushStatus, pullStatus;
-	
+	EditText editText1;
+	Button buttonStoM, buttonMerge, buttonMtoS, buttonViewDB;
+	Account myAccount;
+	ContentResolver mResolver;
+	ContactsObserver observer;
+
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
-		
-		//	TouchDB Connection
-		// start TouchDB
-	    TDServer server = null;
-	    String filesDir = getFilesDir().getAbsolutePath();
-	    try {
-	      server = new TDServer(filesDir);
-	    } catch (IOException e) {
-	      Log.e("tag", "Error starting TDServer", e);
-	    }
 
-	    // start TouchDB-Ektorp adapter
-	    HttpClient httpClient = new TouchDBHttpClient(server);
-	    dbInstance = new StdCouchDbInstance(httpClient);
-
-	    // create a local database
-	    dbConnector = dbInstance.createConnector("testdb", true);
-
-	    // push this database to the test replication server
-	    pushCommand = new ReplicationCommand.Builder()
-        	.source("testdb")
-	        .target("http://jerryj3.cloudant.com/a_user1")
-	        .continuous(true)
-	        .build();
-
-	    pushStatus = dbInstance.replicate(pushCommand);
+		Log.i(C.ID, "init");
 	    
-	    // pull this database from the test replication server
-	    pullCommand = new ReplicationCommand.Builder()
-        	.source("http://jerryj3.cloudant.com/a_user1")
-        	.target("testdb")
-	        .continuous(true)
-	        .build();
+		myAccount = createSyncAccount(this);
+		Intent intent = new Intent(this, ContactsSyncAdapterService.class);
+		startService(intent);
 
-	    pullStatus = dbInstance.replicate(pullCommand);
-	    
-	    ChangesCommand cmd = new ChangesCommand.Builder().includeDocs(true).build();
-	    ChangeEventTask task = new ChangeEventTask(dbConnector, cmd);
-	    task.execute();
-	    
-	    //	Create event halder for button1
-		editText1 = (EditText)findViewById(R.id.editText1);
-		editText2 = (EditText)findViewById(R.id.editText2);
-		button1 = (Button)findViewById(R.id.button1);
-		button2 = (Button)findViewById(R.id.button2);
-		button1.setOnTouchListener(new View.OnTouchListener() {
+		mResolver = getContentResolver();
+		observer = new ContactsObserver();
+		mResolver.registerContentObserver(RawContacts.CONTENT_URI, true, observer);
+
+		// Create event halder for button1
+		editText1 = (EditText) findViewById(R.id.editText1);
+		buttonStoM = (Button) findViewById(R.id.buttonStoM);
+		buttonMerge = (Button) findViewById(R.id.buttonMerge);
+		buttonMtoS = (Button) findViewById(R.id.buttonMtoS);
+		buttonViewDB = (Button) findViewById(R.id.buttonViewDB);
+		buttonStoM.setOnTouchListener(new View.OnTouchListener() {
 			@Override
 			public boolean onTouch(View v, MotionEvent event) {
-				if(event.getActionMasked() == MotionEvent.ACTION_DOWN) {
+				if (event.getActionMasked() == MotionEvent.ACTION_DOWN) {
+					return false;
+				}
+
+				editText1.setText("");
+
+				Bundle bundle = new Bundle();
+				bundle.putBoolean(ContentResolver.SYNC_EXTRAS_EXPEDITED, true);
+				bundle.putBoolean(ContentResolver.SYNC_EXTRAS_MANUAL, true);
+				bundle.putInt("syncMode", C.SYNC_SERVER_TO_MOBILE);
+				ContentResolver.requestSync(myAccount, AUTHORITY, bundle);
+
+				return false;
+			}
+		});
+
+		buttonMerge.setOnTouchListener(new View.OnTouchListener() {
+			@Override
+			public boolean onTouch(View v, MotionEvent event) {
+				if (event.getActionMasked() == MotionEvent.ACTION_DOWN) {
 					return false;
 				}
 				
-//				editText1.append("STATUS: " + pushStatus.isOk() + "\n");
-				
-				ObjectMapper om = new ObjectMapper();
-				ObjectNode newDoc = om.createObjectNode();
-				String id = editText2.getText() + "_" + System.currentTimeMillis();
-				ignoreId = id;
-				newDoc.put("fileName", "file_" + System.currentTimeMillis());
-				dbConnector.create(id, newDoc);
-				
-				editText1.append("UP : " + id + "\n");
-				
+				Bundle bundle = new Bundle();
+				bundle.putBoolean(ContentResolver.SYNC_EXTRAS_EXPEDITED, true);
+				bundle.putBoolean(ContentResolver.SYNC_EXTRAS_MANUAL, true);
+				bundle.putInt("syncMode", C.SYNC_MERGE);
+				ContentResolver.requestSync(myAccount, AUTHORITY, bundle);
 				return false;
 			}
 		});
-		
-		button2.setOnTouchListener(new View.OnTouchListener() {
+
+		buttonMtoS.setOnTouchListener(new View.OnTouchListener() {
 			@Override
 			public boolean onTouch(View v, MotionEvent event) {
-				dbInstance.replicate(pushCommand);
-				dbInstance.replicate(pullCommand);
+				if (event.getActionMasked() == MotionEvent.ACTION_DOWN) {
+					return false;
+				}
+
+				editText1.setText("");
+
+				Bundle bundle = new Bundle();
+				bundle.putBoolean(ContentResolver.SYNC_EXTRAS_EXPEDITED, true);
+				bundle.putBoolean(ContentResolver.SYNC_EXTRAS_MANUAL, true);
+				bundle.putInt("syncMode", C.SYNC_MOBILE_TO_SERVER);
+				ContentResolver.requestSync(myAccount, AUTHORITY, bundle);
+				
 				return false;
 			}
 		});
+
+		buttonViewDB.setOnTouchListener(new View.OnTouchListener() {
+			@Override
+			public boolean onTouch(View v, MotionEvent event) {
+				if (event.getActionMasked() == MotionEvent.ACTION_DOWN) {
+					return false;
+				}
+
+				editText1.setText("");
+
+				// List all documents
+				CouchDbConnector dbConnector = ContactsSyncAdapterService.init(getApplicationContext(), myAccount, false);
+				ViewQuery view = new ViewQuery().allDocs().includeDocs(true);
+				ViewResult allRows = dbConnector.queryView(view);
+				Iterator<Row> it = allRows.iterator();
+				while(it.hasNext()) {
+					Row node = it.next();
+					editText1.append("DOC: " + node.getDocAsNode() + "\n");
+				}
+				
+				return false;
+			}
+		});
+
+		Bundle bundle = new Bundle();
+		bundle.putBoolean(ContentResolver.SYNC_EXTRAS_EXPEDITED, true);
+		bundle.putBoolean(ContentResolver.SYNC_EXTRAS_MANUAL, true);
+		bundle.putInt("syncMode", C.SYNC_INIT);
+		ContentResolver.requestSync(myAccount, AUTHORITY, bundle);
+	}
+
+	protected void managedQuery(Object object, Object object2, Object object3, Object object4) {
+		// TODO Auto-generated method stub
 
 	}
 
@@ -131,19 +149,92 @@ public class MainActivity extends Activity {
 		getMenuInflater().inflate(R.menu.main, menu);
 		return true;
 	}
-	
-	public class ChangeEventTask extends ChangesFeedAsyncTask {
-		public ChangeEventTask(CouchDbConnector couchDbConnector, ChangesCommand changesCommand) {
-			super(couchDbConnector, changesCommand);
+
+	/**
+	 * Create a new dummy account for the sync adapter
+	 * 
+	 * @param context
+	 *            The application context
+	 */
+	// The authority for the sync adapter's content provider
+	public static final String AUTHORITY = "com.android.contacts";
+	// An account type, in the form of a domain name
+	public static final String ACCOUNT_TYPE = "com.example.helloworld";
+	// The account name
+	public static final String ACCOUNT = "dummyaccount";
+
+	public static Account createSyncAccount(Context context) {
+		// Create the account type and default account
+		Account newAccount = new Account(ACCOUNT, ACCOUNT_TYPE);
+		// Get an instance of the Android account manager
+		AccountManager accountManager = (AccountManager) context.getSystemService(ACCOUNT_SERVICE);
+		/*
+		 * Add the account and account type, no password or user data If
+		 * successful, return the Account object, otherwise report an error.
+		 */
+		if (accountManager.addAccountExplicitly(newAccount, null, null)) {
+			/*
+			 * If you don't set android:syncable="true" in in your <provider>
+			 * element in the manifest, then call context.setIsSyncable(account,
+			 * AUTHORITY, 1) here.
+			 */
+		} else {
+			/*
+			 * The account exists or some other error occurred. Log this, report
+			 * it, or handle it internally.
+			 */
 		}
 
-		@Override
-		protected void handleDocumentChange(DocumentChange change) {
-			JsonNode doc = change.getDocAsNode();
-			String id = doc.get("_id").asText();
-			if(!id.equals(ignoreId)) {
-				editText1.append("DOWN: " + id + "\n");
-			}
+		return newAccount;
+	}
+
+	// https://www.grokkingandroid.com/use-contentobserver-to-listen-to-changes/
+	// Description about handler...
+	public class ContactsObserver extends ContentObserver {
+		public ContactsObserver() {
+			super(null);
+			// TODO Auto-generated constructor stub
 		}
+
+		/*
+		 * Define a method that's called when data in the observed content
+		 * provider changes. This method signature is provided for compatibility
+		 * with older platforms.
+		 */
+		@Override
+		public void onChange(boolean selfChange) {
+			/*
+			 * Invoke the method signature available as of Android platform
+			 * version 4.1, with a null URI.
+			 */
+			onChange(selfChange, null);
+		}
+
+		/*
+		 * Define a method that's called when data in the observed content
+		 * provider changes.
+		 */
+		@Override
+		public void onChange(boolean selfChange, Uri changeUri) {
+			Log.e(C.ID, "onChange");
+			Log.e(C.ID, "selfChange: " + selfChange);
+			/*
+			 * Ask the framework to run your sync adapter. To maintain backward
+			 * compatibility, assume that changeUri is null.
+			 */
+			Bundle bundle = new Bundle();
+			bundle.putBoolean(ContentResolver.SYNC_EXTRAS_EXPEDITED, true);
+			bundle.putBoolean(ContentResolver.SYNC_EXTRAS_MANUAL, true);
+			bundle.putInt("syncMode", C.SYNC_MOBILE_TO_SERVER);
+			ContentResolver.requestSync(myAccount, AUTHORITY, bundle);
+		}
+	}
+
+	@Override
+	protected void onDestroy() {
+		// TODO Auto-generated method stub
+		super.onDestroy();
+
+		mResolver.unregisterContentObserver(observer);
 	}
 }
